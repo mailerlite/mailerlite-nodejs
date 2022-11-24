6d9643a001d48ada417f479a7d4cb14e8e10c028
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2022-11-24 14:23:30 +0200

    request() function for fetching data from API

---
+++ src/fetch.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { Config }  from './modules/types'
+
+type Options = {
+    headers?: object;
+    method?: string;
+    body?: null | object;
+    params?: object;
+}
+
+export default function request(endpoint: string = "", options: Options = {}, config: Config) {
+
+    const {headers = {}, method = 'GET', body = null, params = {}} = options
+
+    let queryString = serializeQuery(params)
+    queryString = queryString ? `?${queryString}` : ''
+
+    return axios({
+        method: method,
+        url: config.basePath + endpoint + queryString,
+        headers: {
+            ...headers,
+            "Authorization": `Bearer ${config.api_key}`,
+            "X-Requested-With": "XMLHttpRequest",
+            "Content-type": "application/json",
+            "accept-encoding": "null" // needed for axios
+        },
+        data: body && JSON.stringify(body)
+    })
+}
+
+function serializeQuery(params: object, prefix?: string): string {
+
+    const query: string[] = Object.keys(params).map((key) => {
+        const value = params[key as keyof typeof params];
+
+        if (params.constructor === Array)
+            key = `${prefix}[]`;
+        else if (params.constructor === Object)
+            key = (prefix ? `${prefix}[${key}]` : key);
+
+        if (typeof value === 'object')
+            return serializeQuery(value, key);
+        else
+            return `${key}=${encodeURIComponent(value)}`;
+    });
+
+    // @ts-ignore
+    return [].concat.apply([], query).join('&')
+}
