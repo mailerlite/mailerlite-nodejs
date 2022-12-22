edcb35285c9199e08eed6bc6429e322c2d4a1bcf
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2022-12-22 09:42:21 +0200

    Including forms class & updated frontpage docs

--- src/MailerLite.ts
+++ src/MailerLite.ts
@@ -6,11 +6,15 @@ import { SubscriberInterface } from "./modules/subscribers/subscribers.types";
 import Campaign from './modules/campaigns/campaigns.module.js';
 import { CampaignsInterface } from "./modules/campaigns/campaigns.types";
 
+import Form from './modules/forms/forms.module.js';
+import { FormsInterface } from "./modules/forms/forms.types";
+
 export default class MailerLite {
 
     private config: Config;
     public subscribers: SubscriberInterface;
     public campaigns: CampaignsInterface;
+    public forms: FormsInterface;
 
     constructor(params: { api_key: string; }) {
         this.config = {
@@ -20,5 +24,6 @@ export default class MailerLite {
 
         this.subscribers = new Subscriber(this.config);
         this.campaigns = new Campaign(this.config);
+        this.forms = new Form(this.config);
     }
 };
