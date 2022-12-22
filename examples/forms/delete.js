110cbec08fc9e41dc14308fad1c96f6f74268f21
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2022-12-22 09:43:19 +0200

    Node scripts to test forms endpoints

--- examples/forms/delete.js
+++ examples/forms/delete.js
@@ -1,4 +1,4 @@
-// node examples/subscribers/find.js ID_string
+// node examples/forms/delete.js ID_string
 
 "use strict";
 
@@ -11,12 +11,12 @@ const mailerlite = new MailerLite({
   api_key: process.env.API_KEY,
 });
 
-let subscriber_id = null
+let form_id = null
 if (process.argv.slice(2).length) {
-  subscriber_id = process.argv[2];
+  form_id = process.argv[2];
 }
 
-mailerlite.subscribers.find(subscriber_id)
+mailerlite.forms.delete(form_id)
   .then(response => {
     console.log(response.data);
   })
---
+++ examples/forms/get.js
@@ -0,0 +1,29 @@
+// node examples/forms/get.js popup name 5 1
+
+"use strict";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import MailerLite from '../../dist/MailerLite.js';
+
+const mailerlite = new MailerLite({
+  api_key: process.env.API_KEY,
+});
+
+let formType = null;
+let params = {};
+
+if (process.argv.slice(2).length) formType = String(process.argv[2]);
+if (process.argv.slice(3).length) params.sort = String(process.argv[3]);
+if (process.argv.slice(4).length) params.limit = parseInt(process.argv[4]);
+if (process.argv.slice(5).length) params.page = parseInt(process.argv[5]);
+
+mailerlite.forms.get(formType, params)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    if (error.response) console.log(error.response.data);
+    console.log(error.message);
+  });
