9e2e5358d5e07079bbacce9b8b8ac2fb7bd60a07
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2022-12-22 09:27:00 +0200

    Added forms endpoints, types & docs

---
+++ src/modules/forms/forms.module.ts
@@ -0,0 +1,63 @@
+import { validateId } from '../validation.js';
+import request from '../../fetch.js'
+
+import type { Config }  from '../types'
+import { AxiosResponse } from "axios";
+import type { FormsInterface, GetParams, ListAllResponse, SingleFormResponse, UpdateParams, FormTypes } from './forms.types.js';
+
+export default class Form implements FormsInterface {
+    private config: Config;
+
+    constructor(config: Config) {
+        this.config = config;
+    }
+
+    /**
+     * @description List all forms
+     *
+     * @see https://developers.mailerlite.com/docs/forms.html#list-all-forms
+     *
+     * @type {String} - Form type
+     * @params {Object} - List forms params
+     */
+    public get(type: FormTypes, params: GetParams): Promise<AxiosResponse<ListAllResponse>> {
+        return request(`/api/forms/${type}`, {
+            method: "GET",
+            params: params
+        }, this.config);
+    }
+
+    /**
+     * @description Update a form
+     *
+     * @see https://developers.mailerlite.com/docs/forms.html#update-a-form
+     *
+     * @form_id {String} - Form ID
+     * @requestBody {Object} - Form data for update
+     */
+    public update(form_id: string, requestBody: UpdateParams): Promise<AxiosResponse<SingleFormResponse, UpdateParams>> {
+
+        validateId(form_id);
+
+        return request(`/api/forms/${form_id}`, {
+            method: "PUT",
+            body: requestBody
+        }, this.config);
+    }
+
+    /**
+     * @description Delete a form
+     *
+     * @see https://developers.mailerlite.com/docs/forms.html#delete-a-form
+     *
+     * @form_id {String} - Form ID
+     */
+    public delete(form_id: string): Promise<AxiosResponse<null>> {
+
+        validateId(form_id);
+
+        return request(`/api/forms/${form_id}`, {
+            method: "DELETE"
+        }, this.config);
+    }
+};
