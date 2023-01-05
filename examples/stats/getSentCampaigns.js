146f0463d9c638a89c95ba3cef343c508e0e84d9
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2023-01-05 12:38:13 +0200

    Node scripts to test campaigns stats endpoints

---
+++ examples/stats/getSentCampaigns.js
@@ -0,0 +1,31 @@
+// node examples/stats/getSentCampaigns.js regular 25 1
+
+"use strict";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import MailerLite from '../../dist/MailerLite.js';
+
+const mailerlite = new MailerLite({
+  api_key: process.env.API_KEY,
+});
+
+let params = {
+  filter: {
+    status: "sent"
+  }
+};
+
+if (process.argv.slice(2).length) params.filter.type = String(process.argv[2]);
+if (process.argv.slice(4).length) params.limit = parseInt(process.argv[3]);
+if (process.argv.slice(5).length) params.page = parseInt(process.argv[4]);
+
+mailerlite.stats.getSentCampaigns(params)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    if (error.response) console.log(error.response.data);
+    console.log(error.message);
+  });
