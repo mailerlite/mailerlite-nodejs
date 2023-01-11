295f0844bf5f91a4c4ab8b0cdcbf87ca06043ff3
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2023-01-11 10:04:07 +0200

    Node scripts to test automation stats endpoints

---
+++ examples/stats/getAutomations.js
@@ -0,0 +1,31 @@
+// node examples/stats/getAutomations.js
+
+"use strict";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import MailerLite from '../../dist/MailerLite.js';
+
+const mailerlite = new MailerLite({
+  api_key: process.env.API_KEY,
+});
+
+const params = {
+  filter: {
+    status: true,
+    name: "nodejs",
+    // group: "GROUP_ID"
+  },
+  limit: 10,
+  page: 1
+};
+
+mailerlite.stats.getAutomations(params)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    if (error.response) console.log(error.response.data);
+    console.log(error.message);
+  });
