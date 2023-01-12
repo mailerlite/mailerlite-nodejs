d2bea3fa1add980a96b05433444840e0fed41a6d
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2023-01-12 13:32:26 +0200

    Node scripts to test batches endpoint

---
+++ examples/batches/send.js
@@ -0,0 +1,51 @@
+// node examples/batches/send.js
+
+"use strict";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import MailerLite from '../../dist/MailerLite.js';
+
+const mailerlite = new MailerLite({
+  api_key: process.env.API_KEY,
+});
+
+const params = {
+  requests: [
+    {
+      method: "POST",
+      path:   "api/fields",
+      body:   {
+        name: "test batch field 3",
+        type: "text"
+      }
+    },
+    {
+      method: "POST",
+      path:   "api/fields",
+      body:   {
+        name: "test batch field 4",
+        type: "text"
+      }
+    },
+    {
+      method: "GET",
+      path:   "/api/forms/popup",
+      body:   {
+        filter: {
+          name: "nodejs"
+        }
+      }
+    }
+  ]
+}
+
+mailerlite.batches.send(params)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    if (error.response) console.log(error.response.data);
+    console.log(error.message);
+  });
