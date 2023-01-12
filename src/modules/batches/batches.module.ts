8d2371aa76427bec27614ab9c5ad71805db7439b
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2023-01-12 13:30:49 +0200

    Added batches endpoint, types, tests & docs

---
+++ src/modules/batches/batches.module.ts
@@ -0,0 +1,26 @@
+import request from '../../fetch.js'
+
+import type { Config }  from '../types'
+import { AxiosResponse } from "axios";
+import { BatchResponses, BatchesInterface, BatchParams } from "./batches.types.js";
+
+export default class Batch implements BatchesInterface {
+    private config: Config;
+
+    constructor(config: Config) {
+        this.config = config;
+    }
+
+    /**
+     * @description Make multiple request to api in a single call
+     *
+     * @see https://developers.mailerlite.com/docs/batching.html
+     *
+     */
+    public send(requestBody: BatchParams): Promise<AxiosResponse<BatchResponses>> {
+        return request(`/api/batch`, {
+            method: "POST",
+            body: requestBody
+        }, this.config);
+    }
+};
