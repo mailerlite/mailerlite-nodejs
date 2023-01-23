95da596432aa1b5ebbdb1db923bae273bcf9588d
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2023-01-23 16:28:09 +0200

    Node scripts to test webhooks endpoints

---
+++ examples/webhooks/get.js
@@ -0,0 +1,21 @@
+// node examples/webhooks/get.js
+
+"use strict";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import MailerLite from '../../dist/MailerLite.js';
+
+const mailerlite = new MailerLite({
+  api_key: process.env.API_KEY,
+});
+
+mailerlite.webhooks.get()
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    if (error.response) console.log(error.response.data);
+    console.log(error.message);
+  });
