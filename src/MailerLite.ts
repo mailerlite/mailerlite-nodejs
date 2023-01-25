635a51147c89dc9e32e3a70130dcda0d64f580b2
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2023-01-25 16:42:47 +0200

    Reorder to be aligned with docs

--- src/MailerLite.ts
+++ src/MailerLite.ts
@@ -3,12 +3,6 @@ import { Config }  from './modules/types'
 import Subscriber from './modules/subscribers/subscribers.module.js';
 import { SubscriberInterface } from "./modules/subscribers/subscribers.types";
 
-import Campaign from './modules/campaigns/campaigns.module.js';
-import { CampaignsInterface } from "./modules/campaigns/campaigns.types";
-
-import Form from './modules/forms/forms.module.js';
-import { FormsInterface } from "./modules/forms/forms.types";
-
 import Group from './modules/groups/groups.module.js';
 import { GroupsInterface } from "./modules/groups/groups.types";
 
@@ -21,17 +15,23 @@ import { FieldsInterface } from "./modules/fields/fields.types";
 import Automation from './modules/automations/automations.module.js';
 import { AutomationsInterface } from "./modules/automations/automations.types";
 
-import Timezone from './modules/timezones/timezones.module.js';
-import { TimezonesInterface } from "./modules/timezones/timezones.types";
+import Campaign from './modules/campaigns/campaigns.module.js';
+import { CampaignsInterface } from "./modules/campaigns/campaigns.types";
 
-import Language from './modules/languages/languages.module.js';
-import { LanguagesInterface } from "./modules/languages/languages.types";
+import Form from './modules/forms/forms.module.js';
+import { FormsInterface } from "./modules/forms/forms.types";
 
 import Batch from './modules/batches/batches.module.js';
 import { BatchesInterface } from "./modules/batches/batches.types";
 
 import Webhook from './modules/webhooks/webhooks.module.js';
 import { WebhooksInterface } from "./modules/webhooks/webhooks.types";
+
+import Timezone from './modules/timezones/timezones.module.js';
+import { TimezonesInterface } from "./modules/timezones/timezones.types";
+
+import Language from './modules/languages/languages.module.js';
+import { LanguagesInterface } from "./modules/languages/languages.types";
 
 import Statistics from './modules/stats/stats.module.js';
 import { StatsInterface } from "./modules/stats/stats.types";
@@ -40,16 +40,16 @@ export default class MailerLite {
 
     private config: Config;
     public subscribers: SubscriberInterface;
-    public campaigns: CampaignsInterface;
-    public forms: FormsInterface;
     public groups: GroupsInterface;
     public segments: SegmentsInterface;
     public fields: FieldsInterface;
     public automations: AutomationsInterface;
-    public timezones: TimezonesInterface;
-    public languages: LanguagesInterface;
+    public campaigns: CampaignsInterface;
+    public forms: FormsInterface;
     public batches: BatchesInterface;
     public webhooks: WebhooksInterface;
+    public timezones: TimezonesInterface;
+    public languages: LanguagesInterface;
     public stats: StatsInterface;
 
     constructor(params: { api_key: string; }) {
@@ -59,16 +59,16 @@ export default class MailerLite {
         };
 
         this.subscribers = new Subscriber(this.config);
-        this.campaigns = new Campaign(this.config);
-        this.forms = new Form(this.config);
         this.groups = new Group(this.config);
         this.segments = new Segment(this.config);
         this.fields = new Field(this.config);
         this.automations = new Automation(this.config);
-        this.timezones = new Timezone(this.config);
-        this.languages = new Language(this.config);
+        this.campaigns = new Campaign(this.config);
+        this.forms = new Form(this.config);
         this.batches = new Batch(this.config);
         this.webhooks = new Webhook(this.config);
+        this.timezones = new Timezone(this.config);
+        this.languages = new Language(this.config);
         this.stats = new Statistics(this.config);
     }
 };
