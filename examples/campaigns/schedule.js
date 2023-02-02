e4cfc7cca836e32b45ae602a5469c55aabfabb77
Mantas97 <38210196+Mantas97@users.noreply.github.com>
2023-02-02 17:44:00 +0200

    index.mjs path changed

--- examples/campaigns/schedule.js
+++ examples/campaigns/schedule.js
@@ -3,9 +3,9 @@
 "use strict";
 
 import dotenv from 'dotenv';
-dotenv.config();
+import MailerLite from '../../dist/index.mjs';
 
-import MailerLite from '../../dist/esm/index.mjs';
+dotenv.config();
 
 const mailerlite = new MailerLite({
   api_key: process.env.API_KEY,
